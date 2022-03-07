bdef779ea7409daca0430d9be715da0041477d1b
destiny <1848765519@qq.com>
2022-03-07 17:08:28 +0800

    feat: 优化 autoComplete, 增加获取焦点和失去焦点逻辑

--- src/autoComplete/index.js
+++ src/autoComplete/index.js
@@ -1,8 +1,18 @@
-import { fromEvent, debounceTime, switchMap, map, filter } from 'rxjs';
+import {
+  fromEvent,
+  debounceTime,
+  switchMap,
+  map,
+  filter,
+  from,
+  withLatestFrom,
+  connectable,
+} from 'rxjs';
+
 const url = 'https://zh.wikipedia.org/w/api.php?action=opensearch&format=json&limit=5&origin=*';
 
 const getSuggestList = (keyword) =>
-  fetch(url + '&search=' + keyword, { method: 'GET', mode: 'cors' }).then((res) => res.json());
+  fetch(url + '&search=' + keyword, {method: 'GET', mode: 'cors'}).then((res) => res.json());
 
 const searchDOM = document.getElementById('search');
 const listDOM = document.getElementById('suggest-list');
@@ -13,19 +23,46 @@ const render = (suggestArr = []) => {
 const keyword = fromEvent(searchDOM, 'input');
 const selectItem = fromEvent(listDOM, 'click');
 
-keyword
-  .pipe(
-    debounceTime(300),
-    switchMap((e) => getSuggestList(e.target.value)).pipe(map((e, res) => res[1]))
+const searchObservable = connectable(keyword.pipe(
+  debounceTime(300), // 防抖
+  switchMap(
+    // 每次的输入转化为每个请求, 但是只需要最新的请求即可, 所以使用 switch
+    e => from(getSuggestList(e.target.value)).pipe(
+      // 每个请求映射成返回的结果列表
+      map(res => res[1])
+    )
   )
-  .subscribe((list) => render(list));
+));
+searchObservable.subscribe({
+  next: render
+})
 
-selectItem
-  .pipe(
-    filter((e) => e.target.matches('li')),
-    map((e) => e.target.innerText)
-  )
-  .subscribe((value) => {
+searchObservable.connect();
+
+selectItem.pipe(
+  // 只需要选中的元素是 li
+  filter(e => {
+    return e.target.matches("li")
+  }),
+  map(e => e.target.innerText)
+).subscribe({
+  next: (value) => {
     searchDOM.value = value;
-    render();
-  });
+  }
+})
+const focus = fromEvent(searchDOM, "focus");
+const blur = fromEvent(document, "click");
+
+focus.pipe(
+  withLatestFrom(searchObservable),
+  map(([, list]) => list)
+).subscribe({
+  next: render
+})
+
+blur.pipe(filter(e => e.target !== searchDOM))
+  .subscribe({
+    next: () => {
+      render()
+    }
+  })
